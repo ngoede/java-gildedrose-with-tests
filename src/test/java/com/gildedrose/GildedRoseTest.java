94998645a205ae5c39230663bc634bd753564903
Nick Goede <nick@industriallogic.com>
2021-08-19 16:13:54 -0400

    normal item tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,32 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void normalItemsWithPositiveSellInDecreaseQualityByOne() {
+        Item item = new Item("any normal item", 1, 100);
+        GildedRose subject = new GildedRose(new Item[]{item});
+        subject.updateQuality();
+        assertEquals(99, subject.items[0].quality);
+    }
+
+    @Test
+    void normalItemsWithZeroSellInDecreaseQualityByTwo() {
+        Item item = new Item("any normal item", 0, 100);
+        GildedRose subject = new GildedRose(new Item[]{item});
+        subject.updateQuality();
+        assertEquals(98, subject.items[0].quality);
+    }
+
+    @Test
+    void normalItemsWithNegativeSellInDecreaseQualityByTwo() {
+        Item item = new Item("any normal item", 0, 100);
+        GildedRose subject = new GildedRose(new Item[]{item});
+        subject.updateQuality();
+        assertEquals(98, subject.items[0].quality);
     }
 
 }
